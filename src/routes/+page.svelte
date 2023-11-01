ba4ec2001146528d96d946ab1d06c0e20311f032
stanleymw <107821509+stanleymw@users.noreply.github.com>
2023-10-31 20:55:00 -0700

    initial commit v2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,65 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Main from './main.svelte';
+
+  let webhook_id = "";
+  $: real = webhook_id.trim();
+
+  let success = false;
+  
+  async function getWebhook(url) {
+    if (!url.startsWith("https://discord.com/api/webhooks/")) {
+      success = false
+      throw new Error("Must be a Discord Webhook URL!")
+    }
+
+    const res = await fetch(url);
+    if (res.ok) {
+      success = true;
+      return res.json();
+    }
+    success = false;
+    throw new Error('Request Failed');
+  };
+
+  $: prom = getWebhook(real);
+
+</script>
+
+<div class="flex-container">
+  <h1>Stanley's Discord Webhook Frontend</h1>
+
+  <label id="webhook_box">
+    <input type="string" bind:value={webhook_id} placeholder="https://discord.com/api/webhooks/xxxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxxx">
+  </label>
+</div>
+
+{#await prom}
+  <code>Resolving webhook...</code>
+{:then ret}
+  <code>Webhook resolved.</code>
+  <div>
+    <Main webhook_obj={ret}/>
+  </div>
+{:catch}
+  <code>Error in resolving webhook. Check the URL</code>
+{/await}
+
+<style>
+  .flex-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  #webhook_box {
+    width: 100%;
+  }
+
+  input {
+    width: 95%;
+  }
+</style>
---
+++ src/routes/main.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let webhook_obj = {};
+
+  let msg = "";
+</script>
+
+<div class="main-form">
+  <code>
+    Webhook Name: {webhook_obj['name']} <br>
+    Server ID: {webhook_obj['guild_id']} <br>
+    Channel ID: {webhook_obj['channel_id']} <br>
+  </code>
+
+  <form>
+    <input type="text" placeholder="Enter a message to send..." bind:value={msg}>
+
+    <button on:click={() => {
+      fetch(webhook_obj['url'], {
+        method: "POST",
+
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        },
+
+        body: JSON.stringify({
+          content: msg
+        })
+      })
+    }}>Send Message</button>
+  </form>
+</div>
+
+<style>
+  .main-form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
